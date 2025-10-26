42b03e7ea7e7219fd55ab56df145427e1bee285a
尼古拉斯 <lintznicolas2@gmail.com>
2025-10-26 15:13:22 -0300

    Primer commit: Estructura Django

--- mesas/models.py
+++ mesas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Mesa(models.Model):
+    numero = models.PositiveIntegerField(unique=True)
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"Mesa {self.numero}"
